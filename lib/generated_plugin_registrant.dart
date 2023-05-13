290d7bf4815ea771e4df0ff971f18865c8ad6357
unknown <bayecheikhfall249@gmail.com>
2023-05-13 18:07:42 +0000

    permission pour ios

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   ImagePickerPlugin.registerWith(registrar);
